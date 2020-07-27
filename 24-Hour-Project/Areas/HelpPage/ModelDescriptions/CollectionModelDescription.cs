4fe984c2ba01ba6f32856a22502d490263b73fc8
AThooker <austintrent@icloud.com>
2020-07-27 15:59:51 -0400

    first

---
+++ 24-Hour-Project/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _24_Hour_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ 24-Hour-Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24_Hour_Project.WebApiApplication" Language="C#" %>
