546e4474db89fab3bc49b9289935ff8229917f09
AThooker <austintrent@icloud.com>
2020-07-28 09:36:08 -0400

    post info from Kaleb

---
+++ 24-Hour-Project/Controllers/PostController.cs
@@ -0,0 +1,41 @@
+﻿using _24HourProject.Models;
+using _24HourProject.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24_Hour_Project.Controllers
+{
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+
+        public IHttpActionResult Post(CreatePost post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+            if (!service.PostCreate(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+    }
+}
--- 24HourProject.Models/CreatePost.cs
+++ 24HourProject.Models/CreatePost.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _24HourProject.Models
 {
-    class CreatePost
+    public class CreatePost
     {
+        //What data to we need to CAPTURE when a post is created?
+        //no ID is needed here because .service and .data will take care of that.
+        [Required]
+
+        [MaxLength(200, ErrorMessage = "There are too many characters entered.")]
+        public string Title { get; set; }
+        [MaxLength(1000)]
+        public string Text { get; set; }
     }
 }
