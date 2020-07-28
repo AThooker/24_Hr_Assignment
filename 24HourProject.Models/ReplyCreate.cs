33beda10bacbe2eb082f9a76de49c13aeeb7018e
Lisa Jeffers <lisajeffers0106@gmail.com>
2020-07-28 10:39:29 -0400

    Testing Clone

--- 24HourProject.Models/ReplyCreate.cs
+++ 24HourProject.Models/ReplyCreate.cs
@@ -9,6 +9,7 @@ namespace _24HourProject.Models
 {
     class ReplyCreate
     {
+        // This is the Reply Create Model
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
         [MaxLength(2000, ErrorMessage = "There are too many characters in this field.")]
