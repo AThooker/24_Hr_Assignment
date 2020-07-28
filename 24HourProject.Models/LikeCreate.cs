13b6809710df66bb8d87d9715e19ba2210d094d3
kcprater <kristopherprater@gmail.com>
2020-07-28 09:16:18 -0400

    likes

---
+++ 24HourProject.Models/LikeCreate.cs
@@ -0,0 +1,26 @@
+﻿using _24HourProject.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Models
+{
+    public class LikeCreate
+    {
+        [Key]
+        public int LikeId { get; set; }
+        [Required]
+        public int PostingId { get; set; }
+        [ForeignKey(nameof(PostingId))]
+        public virtual Posting Posting { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [ForeignKey(nameof(OwnerId))]
+        public virtual User Liker { get; set; }
+    }
+}
+
--- 24HourProject.Models/LikeDetail.cs
+++ 24HourProject.Models/LikeDetail.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _24HourProject.Models
 {
-    class ReplyCreate
+    class LikeDetail
     {
+        public int LikeId { get; set; }
+        public int PostingId { get; set; }
+        public Guid OwnerId { get; set; }
     }
 }
